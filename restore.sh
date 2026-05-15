e9cc72886de83ad7278512dc9a50f180cd75707e
HeriKeyzenlocker <herikeyzenlocker@gmail.com>
2026-05-15 21:19:13 +0700

    Fix restore script: tambah neofetch, atasi folder existing

--- restore.sh
+++ restore.sh
@@ -6,28 +6,33 @@ echo "🔧 Memulai pemulihan Termux..."
 # Minta izin akses penyimpanan
 termux-setup-storage
 
-# Update dan install paket yang dibutuhkan
-pkg update -y && pkg install -y git zsh figlet ruby
+# Update dan install paket
+pkg update -y && pkg install -y git zsh figlet ruby neofetch
 
 # Install lolcat
 gem install lolcat
 
-# Clone repository backup
-git clone https://github.com/herikeyzenlocker-sketch/termux-herikeyzenlocker.git /sdcard/termux_backup
+# Clone repository backup (jika belum)
+if [ ! -d /sdcard/termux_backup ]; then
+    git clone https://github.com/herikeyzenlocker-sketch/termux-herikeyzenlocker.git /sdcard/termux_backup
+fi
 
 # Salin file konfigurasi Zsh
 cp /sdcard/termux_backup/.zshrc ~/
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Install Oh My Zsh (jika belum)
+if [ ! -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
-# Pasang plugin auto-suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Pasang plugin auto-suggestions (jika belum)
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
-# Ganti shell default ke Zsh
-chsh -s zsh
+# Ganti shell default ke Zsh (jika belum)
+if [ "$SHELL" != "$(which zsh)" ]; then
+    chsh -s zsh
+fi
 
-# Terapkan konfigurasi
-exec zsh
-
-echo "✅ Pemulihan selesai! Termux-mu sudah kembali seperti semula."
+echo "✅ Pemulihan selesai! Silakan tutup Termux dan buka kembali."
